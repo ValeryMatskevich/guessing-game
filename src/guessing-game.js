3edd25ec2e0f31f31313df4b43e43c1441348cf9
Valery Matskevich <v.matskevich93@gmail.com>
2023-02-21 19:30:48 +0300

    feat:add a solution to the problem

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,24 @@
 class GuessingGame {
-    constructor() {}
+    constructor () {
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.attempt = Math.round((this.min + this.max) / 2);
+        return this.attempt;
     }
 
     lower() {
-
+        this.max = this.attempt;
     }
 
-    greater() {
 
+    greater() {
+        this.min = this.attempt;
     }
 }
 
